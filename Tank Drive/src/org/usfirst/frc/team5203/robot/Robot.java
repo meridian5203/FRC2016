aad027996dfeedba1656dffa5051ed4aa4852dab
Josh Bacon <bacon.josh09@gmail.com>
2016-01-09 21:29:58 -0500

    General Housekeeping

--- Tank Drive/src/org/usfirst/frc/team5203/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team5203/robot/Robot.java
@@ -1,46 +1,32 @@
 package org.usfirst.frc.team5203.robot;
 
 
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.RobotDrive;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically it 
- * contains the code necessary to operate a robot with tank drive.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
-public class Robot extends SampleRobot {
-    RobotDrive myRobot;  // class that handles basic drive operations
-    Joystick leftStick;  // set to ID 1 in DriverStation
-    Joystick rightStick; // set to ID 2 in DriverStation
+public class Robot extends IterativeRobot {
+    RobotDrive myRobot;
+    Joystick controller;
     public Robot() {
         myRobot = new RobotDrive(0, 1);
         myRobot.setExpiration(0.1);
-        leftStick = new Joystick(0);
-        rightStick = new Joystick(1);
+        controller = new Joystick(0);
+        myRobot.setSafetyEnabled(true);
     }
 
-    
     /**
-     * Runs the motors with tank steering.
+     * Runs when the teleop period begins
      */
-    public void operatorControl() {
-        myRobot.setSafetyEnabled(true);
-        while (isOperatorControl() && isEnabled()) {
-        	myRobot.tankDrive(leftStick, rightStick);
-            Timer.delay(0.005);		// wait for a motor update time
-        }
-    }
+    public void TeleopInit() {}
+    /**
+     * Runs when autonomous is run
+     */
+    public void AutonomousInit(){}
+    /**
+     * Runs when robot is disabled
+     */
+    public void DisabledInit(){}
+    
 
 }
