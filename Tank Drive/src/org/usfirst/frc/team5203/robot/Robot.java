555e097b6cc8816ec9babcfdd500074bbe0ce25f
Josh Bacon <meridian5203@gmail.com>
2016-01-13 20:06:54 -0500

    Added some variables to the functions

--- Tank Drive/src/org/usfirst/frc/team5203/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team5203/robot/Robot.java
@@ -10,57 +10,46 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.TalonSRX;
 
 public class Robot extends IterativeRobot {
     RobotDrive robot;
     Joystick controller;
+    TalonSRX[] launchWheels = new TalonSRX[2];
+    Solenoid[] solenoid = new Solenoid[1];
     public Compressor compressor = new Compressor();
     public Robot() {
         robot = new RobotDrive(0, 1);
         robot.setExpiration(0.1);
         controller = new Joystick(0);
         robot.setSafetyEnabled(true);
+        solenoid[0] = new Solenoid(0);
     }
 
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#teleopInit()
-     */
+    public void robotInit(){
+    launchWheels[0] = new TalonSRX(Variables.leftMotorPort);	
+    launchWheels[1] = new TalonSRX(Variables.rightMotorPort);
+    }
     public void teleopInit() {
     	TeleOp.init(robot);
     }
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#teleopPeriodic()
-     */
     public void teleopPeriodic(){
-    	TeleOp.update(robot, compressor);
+    	TeleOp.update(robot, compressor, launchWheels, solenoid, controller);
     }
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#autonomousInit()
-     */
     public void autonomousInit(){
     	Autonomous.init(robot);
     }
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#autonomousPeriodic()
-     */
     public void autonomousPeriodic(){
     	Autonomous.update(robot);
     }
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#disabledInit()
-     */
     public void disabledInit(){
     	Disabled.init(robot);
     }
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#testInit()
-     */
+
     public void testInit(){
     	Test.init(robot);
     }
-    /* (non-Javadoc)
-     * @see edu.wpi.first.wpilibj.IterativeRobot#testPeriodic()
-     */
     public void testPeriodic(){
     	Test.update(robot);
     }
